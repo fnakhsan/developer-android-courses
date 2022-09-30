53d2b314531f5a1fbee0a14606fab37756357e50
S[in]S <79033189+fnakhsan@users.noreply.github.com>
2022-09-30 21:53:06 +0700

    Delete Android Basics in Kotlin/Layouts/2-Affirmations/Affirmations/src directory

--- Android Basics in Kotlin/Layouts/2-Affirmations/Affirmations/src/main/java/com/example/affirmations/model/Affirmation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.affirmations.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmation(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
-)
